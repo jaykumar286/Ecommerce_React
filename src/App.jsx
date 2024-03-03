f5c7edae3c53e09c2dee417a2bb8abb694b6f7ee
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-03-03 14:57:22 +0530

    configure reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Button } from "reactstrap";
 
 function App() {
   return (
     <>
+      <h1>Shopcart</h1>
+      <Button color="primary">Click Me</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
